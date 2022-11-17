00695e29531a7e2d2e730b760c9cbfb83dd43baf
CALVINCE <calvince.ogwel@gmail.com>
2022-11-17 14:33:12 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(HELLO) {
+    return HELLO.toUpperCase();
+}
+function whisper(hello) {
+    return hello.toLowerCase();
+}
+function logShout(HELLO) {
+    console.log(HELLO.toUpperCase());
+}
+function logWhisper(hello) {
+    console.log(hello.toLowerCase());
+}
+
+
+function sayHiToHeadphonedRoommate (hello) {
+    var cantUnswer = "I can't hear you!";
+    var yesUnswer = "YES INDEED!";
+    var lovUnswer = "I would love to!";
+    if (hello.toLowerCase(hello) === hello) {
+      return cantUnswer;
+    }
+    else if (hello.toUpperCase(hello) === hello) {
+      return yesUnswer;
+    }
+    else if ("Let's have dinner together!" === hello) {
+      return lovUnswer
+    }
+  }
